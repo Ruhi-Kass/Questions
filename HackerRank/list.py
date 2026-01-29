099ea4847521c7edbf314834bb45dd4cc8e7bfbb
Ruhama Kassahun <140911522+Ruhi-Kass@users.noreply.github.com>
2026-01-29 22:01:11 +0300

    Update list.py

--- HackerRank/list.py
+++ HackerRank/list.py
@@ -4,6 +4,9 @@ if __name__ == '__main__':
     z = int(input())
     n = int(input())
     
+     # all [i,j,k] where 0 ≤ i ≤ x, 0 ≤ j ≤ y, 0 ≤ k ≤ z
+    # and i + j + k != n
+    
     result = [[i, j, k] for i in range(x + 1)
                         for j in range(y + 1)
                         for k in range(z + 1)
