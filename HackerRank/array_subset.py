fbe20b5f0a4fd7a55f6a10d9dc6368a7e972afb6
Ruhama Kassahun <140911522+Ruhi-Kass@users.noreply.github.com>
2026-01-29 22:17:15 +0300

    Implement subset checking using Counter

---
+++ HackerRank/array_subset.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+class Solution:
+    def isSubset(self, a, b):
+        count_a = Counter(a)
+        count_b = Counter(b)
+        
+        for num in count_b:
+            if count_b[num] > count_a.get(num, 0):
+                return False
+        return True
